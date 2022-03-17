297800f8661f91a0a2153b8aa268f5a9659836df
andreasbueckle <abueckle@indiana.edu>
2022-03-17 17:03:03 -0400

    Update identify_rui_users.py

--- identify_rui_users.py
+++ identify_rui_users.py
@@ -4,7 +4,7 @@ def main():
 
     # first, let's get the most recent data
     # To get TOKEN for viewing unpublished data, go to EUI, log in, then view source, then copy token from browser
-    TOKEN = "AgE1lb133P2DNnVb8VPyGDzpxOrM2eV51OXkVd9N4NMmn9gr2bIwCEp0p4zgmpnY4bMGo9rDDzlKnncyE63r5cN390"
+    TOKEN = ""
     endpoint = "https://ccf-api.hubmapconsortium.org/v1/hubmap/rui_locations.jsonld"
     headers = {"Authorization": "Bearer " + TOKEN}
     data = requests.get(endpoint, headers=headers).json()
